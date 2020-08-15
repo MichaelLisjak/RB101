9041988ab90e388da2dc247b69097d8c80a7392f
Michael Lisjak <Michael.Lisjak@web.de>
2020-08-15 11:01:55 +0200

    added bonus assignments lizard nad spock to RPS game

--- RPS/rock_paper_scissors.rb
+++ RPS/rock_paper_scissors.rb
@@ -1,43 +1,94 @@
-VALID_PLAYERS = %w(rock paper scissors)
-
+# Variables & Constants
+VALID_PLAYERS = %w(r p sc l sp)
+PLAYER_CHOICES = <<-MSG
+Choose one:
+"r" for rock
+"p" for paper
+"sc" for scissors
+"l" for lizard
+"sp" for spock
+MSG
+score = [0, 0]
+# Method definitions
 def promt(message)
-  Kernel.puts("=> #{message}")
+  puts("=> #{message}")
 end
 
-def win?(first, second)
-  (first == 'rock' && second == 'scissors') ||
-    (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper')
+def display_choice_fully(player, computer)
+  abbreviations = {
+    "r" => "rock",
+    "p" => "paper",
+    "sc" => "scissors",
+    "l" => "lizard",
+    "sp" => "spock"
+  }
+  puts("You chose: #{abbreviations[player]}; " \
+    "Computer chose: #{abbreviations[computer]}")
+end
+
+def beats?(first, second)
+  what_beats_what = {
+    "r" => ["sc", "l"],
+    "p" => ["r", "sp"],
+    "sc" => ["p", "l"],
+    "l" => ["sp", "p"],
+    "sp" => ["sc", "r"]
+  }
+  first_wins = false
+  what_beats_what.each do |k, v|
+    if first == k && v.include?(second)
+      first_wins = true
+    end
+  end
+  return first_wins
 end
 
 def display_results(player, computer)
-  if win?(player, computer)
-    promt("You won!")
-  elsif win?(computer, player)
-    promt("Computer won!")
+  if beats?(player, computer)
+    promt("You won this round!")
+  elsif beats?(computer, player)
+    promt("Computer won this round!")
   else
     promt("it's a tie!")
   end
 end
 
+def declare_grand_winner(player, computer, score)
+  if beats?(player, computer)
+    score[0] += 1
+  elsif beats?(computer, player)
+    score[1] += 1
+  else
+    score[0] += 1
+    score[1] += 1
+  end
+  if score[0] == 2
+    promt("You are the grand winner of the match!")
+    score = 0
+  elsif score[1] == 2
+    promt("the computer is the grand winner of the match")
+    score = 0
+  end
+end
+# Program logic
 loop do
   player = ''
   loop do
-    promt("Choose one: #{VALID_PLAYERS.join(', ')}")
-    player = Kernel.gets().chomp()
+    promt(PLAYER_CHOICES)
+    player = gets.chomp
     if VALID_PLAYERS.include?(player)
       break
     else
       promt("That's not a valid player")
     end
   end
-  computer = VALID_PLAYERS.sample()
+  computer = VALID_PLAYERS.sample
 
-  Kernel.puts("You chose: #{player}; Computer chose: #{computer}")
-
+  display_choice_fully(player, computer)
   display_results(player, computer)
+  declare_grand_winner(player, computer, score)
 
   promt("Do you want to play again?")
-  answer = Kernel.gets().chomp()
-  break unless answer.downcase().start_with?('y')
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
 end
